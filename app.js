1e407e357c4d4b9ed791d6585c5f3e76725c83d2
themiyakasun <themiyakasun.tb@gmail.com>
2023-02-24 23:45:35 +0530

    final

---
+++ app.js
@@ -0,0 +1,6 @@
+const values = document.querySelector('.action');
+const result = document.querySelector('.result');
+const btnContainer = document.querySelectorAll('.button-container');
+
+console.log(values);
+console.log(result);
